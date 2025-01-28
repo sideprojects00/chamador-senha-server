f62e56eb9c0f957929b244cfc123e30fb6e00b5b
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2025-01-28 17:24:31 -0300

    feat: new functions and data

--- index.js
+++ index.js
@@ -1,57 +1,96 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const cors = require('cors');
-require('dotenv').config();
+const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
+const cors = require('cors')
+require('dotenv').config()
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-const app = express();
+const app = express()
 
 app.use(
   cors({
     origin: process.env.FRONT,
-    methods: ['GET', 'POST'],
+    methods: ['GET', 'POST']
   })
-);
+)
 
 app.get('/', (req, res) => {
-  res.send('Servidor Node.js rodando com HTTP!');
-});
+  res.send('Servidor Node.js rodando com HTTP!')
+})
 
-const server = http.createServer(app);
+const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
     origin: process.env.FRONT,
-    methods: ['GET', 'POST'],
-  },
-});
+    methods: ['GET', 'POST']
+  }
+})
 
-let count = 0;
+let filaNormal = 1
+let filaPreferencial = 1
+let senhaAtual = filaNormal
+let tipoAtual = 'Normal'
 
-io.on('connection', (socket) => {
-  console.log('Novo cliente conectado:', socket.id);
+io.on('connection', socket => {
+  console.log('Novo cliente conectado:', socket.id)
 
-  socket.emit('atualizarCount', count);
+  socket.emit('estadoInicial', {
+    senhaAtual,
+    tipoAtual,
+    filaNormal,
+    filaPreferencial
+  })
 
-  socket.on('incrementarCount', () => {
-    count++;
-    console.log('Novo valor do count:', count);
+  socket.on('resetarContadores', () => {
+    filaNormal = 1
+    filaPreferencial = 1
+    senhaAtual = filaNormal
+    tipoAtual = 'Normal'
 
-    io.emit('atualizarCount', count);
-  });
+    console.log('Contadores resetados.')
+    io.emit('estadoAtualizado', {
+      senhaAtual,
+      tipoAtual,
+      filaNormal,
+      filaPreferencial
+    })
+  })
 
-  socket.on('atualizar', (dados) => {
-    console.log('Atualização recebida:', dados);
-    io.emit('atualizacao', dados);
-  });
+  socket.on('chamarProximaNormal', () => {
+    senhaAtual = filaNormal
+    tipoAtual = 'Normal'
+    filaNormal++
+
+    console.log(`Senha normal chamada: ${senhaAtual}`)
+    io.emit('estadoAtualizado', {
+      senhaAtual,
+      tipoAtual,
+      filaNormal,
+      filaPreferencial
+    })
+  })
+
+  socket.on('chamarProximaPreferencial', () => {
+    senhaAtual = filaPreferencial
+    tipoAtual = 'Preferencial'
+    filaPreferencial++
+
+    console.log(`Senha preferencial chamada: ${senhaAtual}`)
+    io.emit('estadoAtualizado', {
+      senhaAtual,
+      tipoAtual,
+      filaNormal,
+      filaPreferencial
+    })
+  })
 
   socket.on('disconnect', () => {
-    console.log('Cliente desconectado:', socket.id);
-  });
-});
+    console.log('Cliente desconectado:', socket.id)
+  })
+})
 
 server.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}...`);
-});
+  console.log(`Servidor rodando na porta ${PORT}...`)
+})
